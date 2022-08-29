97f187ea791e85065e38ccf54f19c1e6aae460cd
AfonsoCalinas <82529055+AfonsoCalinas@users.noreply.github.com>
2022-08-29 15:43:40 +0100

    Update main.py
    Optimizing the way the save path works so that the user only needs to replace the save path in one line (line 7)
    
    Thanks to RainyPT :)

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pytube
 import ffmpeg
 import os
 
-# !!! DON'T FORGET TO CHANGE THE <USER> IN LINE 7, 38, 39, 40 !!!
+# !!! DON'T FORGET TO CHANGE THE <user> IN LINE 7 !!!
 # !!! IF YOU DO ANY CHANGES TO THE FOLDER'S NAME OR ANY OTHER NAME THAT COMPROMISES THE PROJECT'S NEEDED DIRECTORY PATHS, PLEASE DO THOSE CHANGES ON THE CODE ASWELL!!!
-SAVE_PATH = "C:\\Users\\<user>\\Desktop\\YoutubeToMp4\\resources"
+SAVE_PATH = "C:\\Users\\<user>\\Desktop\\YoutubeToMp4"
 
 link = input("> ")
 yt = pytube.YouTube(link)
@@ -31,18 +31,18 @@ for i in reversed(resolution):
 streamAudio = audio.last()
 
 # We download them both with these specific names
-streamVideo.download(output_path=SAVE_PATH, filename='video.mp4')
-streamAudio.download(output_path=SAVE_PATH, filename='audio.mp3')
+streamVideo.download(output_path=SAVE_PATH + '\\resources', filename='video.mp4')
+streamAudio.download(output_path=SAVE_PATH + '\\resources', filename='audio.mp3')
 
 # And finnally merge them together to get the one sweet video you wished for :)
-video_stream = ffmpeg.input('C:\\Users\\<user>\\Desktop\\YoutubeToMp4\\resources\\video.mp4')
-audio_stream = ffmpeg.input('C:\\Users\\<user>\\Desktop\\YoutubeToMp4\\resources\\audio.mp3')
-ffmpeg.output(audio_stream, video_stream, 'C:\\Users\\<user>\\Desktop\\YoutubeToMp4\\output\\coolvideo.mp4').run()
+video_stream = ffmpeg.input(SAVE_PATH + '\\resources\\video.mp4')
+audio_stream = ffmpeg.input(SAVE_PATH + '\\resources\\audio.mp3')
+ffmpeg.output(audio_stream, video_stream, SAVE_PATH + '\\output\\coolvideo.mp4').run()
 
 # Here we're deleting the 2 downloads we did to merge them together so that you don't have unnecessary files rolling around
-if os.path.isfile(SAVE_PATH + '\\video.mp4') and os.path.isfile(SAVE_PATH + '\\audio.mp3'):
-    os.remove(SAVE_PATH + '\\video.mp4')
-    os.remove(SAVE_PATH + '\\audio.mp3')
+if os.path.isfile(SAVE_PATH + '\\resources\\video.mp4') and os.path.isfile(SAVE_PATH + '\\resources\\audio.mp3'):
+    os.remove(SAVE_PATH + '\\resources\\video.mp4')
+    os.remove(SAVE_PATH + '\\resources\\audio.mp3')
     print("> Resources have been deleted and your video is done rendering!\n> Check output folder and retrieve your video!")
 else:
     print("> Resources don't exist!")
